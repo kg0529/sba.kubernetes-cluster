a7773938be75d1ad9b34b443ab12560dd783d3d7
kg0529 <qq610546847@gmail.com>
2021-01-19 03:57:59 -0500

    add update for out-put script

---
+++ kubernetes-cluster-information.sh
@@ -0,0 +1,11 @@
+echo ----------------
+echo     Services
+echo ----------------
+kubectl get services
+echo " "
+echo ----------------
+echo       Pods
+echo ----------------
+kubectl get pods
+echo " "
+echo ----------------
