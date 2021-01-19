5c3b4cede9360b6b14d8b690d6e2b048b830ac11
kg0529 <qq610546847@gmail.com>
2021-01-19 12:50:38 -0500

    modify web,py and re-run the script

--- web.py
+++ web.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
  
 @app.route("/")
@@ -6,4 +7,4 @@ def main():
     return render_template('index.html')
  
 if __name__ == "__main__":
-    app.run(host="localhost", port=9090, debug=True)
+    app.run(host="0.0.0.0", port=9090, debug=True)
