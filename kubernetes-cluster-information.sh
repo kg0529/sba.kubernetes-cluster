0417a700f9589e0d53a379e84b5dacbd9fd20afb
kg0529 <qq610546847@gmail.com>
2021-01-19 04:06:28 -0500

    update out-put script

--- kubernetes-cluster-information.sh
+++ kubernetes-cluster-information.sh
@@ -2,10 +2,20 @@ echo ----------------
 echo     Services
 echo ----------------
 kubectl get services
-echo " "
+kubectl describe service fox-run-app
+echo ""
 echo ----------------
 echo       Pods
 echo ----------------
 kubectl get pods
-echo " "
+echo ""
 echo ----------------
+echo       Nodes
+echo ----------------
+kubectl get nodes
+echo ""
+echo ----------------
+echo    Deloyment
+echo ----------------
+kubetcl get deployments
+kubetcl describe deployment fox-run-deployment
